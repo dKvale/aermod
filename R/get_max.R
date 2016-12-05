ab558d59d1f36dee11d1d8f6c59ec493e8bdb6af
dkvale <kvaled@gmail.com>
2016-12-05 14:27:01 -0600

    drop receptors and projection info tables

--- R/get_max.R
+++ R/get_max.R
@@ -1,12 +1,13 @@
 # Get max dispersion result from AERMOD output
 
-data <- read_aer_out(file_name)
 
-# Get max outside of property boundary
-max_list <- data %>% 
-            mutate(dist_m = round(sqrt((rec_x - src_x)**2 + (rec_y - src_y)**2))) %>%
-            filter(dist_m >= 10) %>%
-            group_by(group, type) %>%  
-            filter(concentration == max(concentration))
+get_max <- function(data,
+                    property_boundary) {
 
-#
+  max_list <- data %>% 
+              dplyr::mutate(dist_m = round(sqrt((rec_x - src_x)**2 + (rec_y - src_y)**2))) %>%
+              dplyr::filter(dist_m >= property_boundary) %>%
+              dplyr::group_by(group, type) %>%  
+              dplyr::filter(concentration == max(concentration))
+
+}
--- README.md
+++ README.md
@@ -1,14 +1,8 @@
 aermod
-
 =======
 
 
-
-
-
 The _aermod_ package provides a data frame interface for performing AERMOD runs from R. 
-
-
 
 
 **AERMOD** is a steady-state plume model that incorporates air dispersion based on planetary boundary layer turbulence and scaling concepts, including treatment of both surface and elevated sources, and both simple and complex terrain.  
