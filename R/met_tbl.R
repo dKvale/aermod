b1af1fb92dc8024e9b00cdd3130b0afb51fcd239
dkvale <kvaled@gmail.com>
2016-12-05 15:49:48 -0600

    split start and end date inputs and add documentation

--- R/met_tbl.R
+++ R/met_tbl.R
@@ -1,30 +1,42 @@
 #' Meteorology options
 #'
 #' Create an input table of AERMOD meteorology options.
-#' @param SURFFILE Surface met data file location.
-#' @param PROFFILE Profile met data file location.
+#' @param surf_file Surface met data file location.
+#' @param surf_site_info Optional surface met site description: \code{"Station_no Year City/Name"}.
+#' @param prof_file Profile met data file location.
+#' @param prof_site_info Optional profile met site description: \code{"Station_no Year City/Name"}.
+#' @param base_elev_m Base elevation above mean sea level of met site in meters.
+#' @param start_met Start date of meteorology data to model. Enter as text: \code{"start_year start_month start_day start_hour"}
+#' @param end_met End date of meteorology data to model. Enter as text : \code{"end_year end_month end_day end_hour"}
 #' @keywords meteorology weather surface aermod input
 #' @export
 #' @examples
-#' met_tbl(SURFFILE = "..\\MET\\RSTMPX5Y.SFC")
+#' met_tbl(surf_file      = "..\\MET\\RSTMPX5Y.SFC",
+#'         surf_site_info = "14925 2006 ROCHESTER/MUNICIPAL_ARPT",
+#'         prof_file      = "..\\MET\\RSTMPX5Y.PFL",
+#'         prof_site_info = "94983 2006 ROCHESTER/MUNICIPAL_ARPT",
+#'         base_elev_m    = 396.0,
+#'         start_met      = "2009 1 1 1",
+#'         end_met        = "2013 12 31 24")
 # 
 #
 
-met_tbl <- function(SURFFILE = "..\\MET\\RSTMPX5Y.SFC",
-                    PROFFILE = "..\\MET\\RSTMPX5Y.PFL",
-                    SURFDATA = "14925 2006 ROCHESTER/MUNICIPAL_ARPT",
-                    UAIRDATA = "94983 2006",
-                    PROFBASE = "396.0 METERS",
-                    STARTEND = "2009 1 1 1, 2013 12 31 24"
+met_tbl <- function(surf_file      = "..\\MET\\RSTMPX5Y.SFC",
+                    surf_site_info = "14925 2006 ROCHESTER/MUNICIPAL_ARPT",
+                    prof_file      = "..\\MET\\RSTMPX5Y.PFL",
+                    prof_site_info = "94983 2006",
+                    base_elev      = 396.0,
+                    start_met      = "2009 1 1 1",
+                    end_met        = "2013 12 31 24"
 ) {
   
-  df <- data.frame(SURFFILE = SURFFILE, 
-                   PROFFILE = PROFFILE,
-                   SURFDATA = SURFDATA,
-                   UAIRDATA = UAIRDATA,
-                   PROFBASE = PROFBASE,
-                   STARTEND = STARTEND,
-                   stringsAsFactors = F)
+  df <- tibble::tibble(surf_file      = surf_file, 
+                       surf_site_info = surf_site_info,
+                       prof_file      = prof_file,
+                       prof_site_info = prof_site_info,
+                       base_elev      = base_elev,
+                       start_met      = start_met,
+                       end_met        = end_met)
   return(df)
 }
 
